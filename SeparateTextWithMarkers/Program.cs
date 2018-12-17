e72cecd9900dde9f5c997ae588f5a4d4e2e0e793
SPlay <SPlay@DESKTOP-U92BQFG>
2018-12-17 18:23:54 -0200

    Adding contents from video

--- SeparateTextWithMarkers/Program.cs
+++ SeparateTextWithMarkers/Program.cs
@@ -6,7 +6,13 @@ namespace SeparateTextWithMarkers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fullName = "SENAI Informática";
+            // This variable holds all the separated parts from the original string variable 'fullName'
+            var nameParts = fullName.Split(' ');
+            foreach (var namePart in nameParts)
+            {
+                Console.WriteLine(namePart);
+            }
         }
     }
 }
