ab4d64cabfed08a4827780c1079375209b1b440a
SPlay <SPlay@DESKTOP-U92BQFG>
2018-12-17 18:22:40 -0200

    Add project files.

---
+++ SeparateTextWithMarkers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeparateTextWithMarkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
