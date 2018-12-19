1de5fd9d51f10b8ac49ed6461917d4046be32001
SPlay <SPlay@DESKTOP-U92BQFG>
2018-12-19 19:37:49 -0200

    Changing solution and adding context for the new video

--- SeparateTextWithMarkers/Program.cs
+++ SeparateTextWithMarkers/Program.cs
@@ -6,12 +6,19 @@ namespace SeparateTextWithMarkers
     {
         static void Main(string[] args)
         {
-            var fullName = "SENAI Informática";
-            // This variable holds all the separated parts from the original string variable 'fullName'
-            var nameParts = fullName.Split(' ');
-            foreach (var namePart in nameParts)
+            var csvContent = "Fruit Name,Price\nOrange,3\nApple,2\nPineapple,4\n";
+            // Separating lines
+            var lines = csvContent.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            //
+            for(int row = 0; row < lines.Length; row++)
             {
-                Console.WriteLine(namePart);
+                // Separating data inside each line
+                var items = lines[row].Split(',');
+                foreach (var item in items)
+                {
+                    Console.Write("{0,16}", item);
+                }
+                Console.WriteLine();
             }
         }
     }
